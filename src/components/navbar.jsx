65756258314892398f201622bfc7c6c303740f96
Atharva Doshi <atharvasdoshi@hotmail.com>
2020-09-07 23:15:38 -0400

    counterapp

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const NavBar = ({totalCounters}) =>
+{
+    return ( <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        Navbar{" "}
+        <span className = "badge badge-pill badge-secondary"> {totalCounters} </span>
+    </a> 
+    </nav>);
+}
+
+ 
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+//import Counters from './components/counters'
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
